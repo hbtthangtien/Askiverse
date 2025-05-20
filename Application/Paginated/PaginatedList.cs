66c344a70a0fec31780d15d732e4e7025ff56a62
Duc Tung <hbtthangtien@gmail.com>
2025-05-20 23:36:45 +0700

    add base request and reponse pattern

--- Application/Paginated/PaginatedList.cs
+++ Application/Paginated/PaginatedList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace Application.Paginated
 {
     public class PaginatedList<T> : List<T>
     {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        private PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+
+        public static PaginatedList<T> Create(IEnumerable<T> source, int count, int pageIndex, int pageSize)
+        {
+            var items = source.ToList();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+
     }
 }
