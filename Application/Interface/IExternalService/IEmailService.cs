f0d53a908668c83ffdb7357c23e0bc1a52639919
minhtran291 <minhtran2912003@gmail.com>
2025-05-24 13:40:42 +0700

    modified connection string, add IEmailService, constant email, link

---
+++ Application/Interface/IExternalService/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interface.IExternalService
+{
+	public interface IEmailService
+	{
+		public Task SendMailAsync(string subject, string body, string email);
+	}
+}
---
+++ Domain/Constants/LinkConstant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Constants
+{
+	public class LinkConstant
+	{
+		public static string baseUri = $"https://localhost:7103";
+
+		public static UriBuilder UriBuilder(string userId, string token, string path)
+		{
+			var builder = new UriBuilder(baseUri);
+			builder.Path = $"account/{path}";
+			builder.Query = $"userId={userId}&token={Uri.EscapeDataString(token)}";
+			return builder ;
+		}
+	}
+}
